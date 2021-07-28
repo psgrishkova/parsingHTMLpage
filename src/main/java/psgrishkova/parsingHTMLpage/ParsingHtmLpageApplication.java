c43d929a66c5eabe8217fedc289f670ef8e3beae
Polina Grishkova <psgrishkova@ya.ru>
2021-07-28 14:29:18 +0400

    Initial commit

---
+++ src/main/java/psgrishkova/parsingHTMLpage/ParsingHtmLpageApplication.java
@@ -0,0 +1,13 @@
+package psgrishkova.parsingHTMLpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParsingHtmLpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParsingHtmLpageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/psgrishkova/parsingHTMLpage/ParsingHtmLpageApplicationTests.java
@@ -0,0 +1,13 @@
+package psgrishkova.parsingHTMLpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParsingHtmLpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
